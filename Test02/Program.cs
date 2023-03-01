46206d71c1c21ffaf675c06ef2589dad819ccf70
Vinicius Jose <viniciuswk@gmail.com>
2023-03-01 18:07:54 -0300

    first commit

---
+++ Test02/Program.cs
@@ -0,0 +1,24 @@
+﻿
+int numero;
+int valorA = 0;
+int valorB = 1;
+int valorC = 0;
+
+Console.Write("Digite um número inteiro: ");
+numero = int.Parse(Console.ReadLine());
+
+while (valorC < numero)
+{
+    valorC = valorA + valorB;
+    valorA = valorB;
+    valorB = valorC;
+}
+
+if (valorC == numero)
+{
+    Console.WriteLine($"O número {numero}, pertence à sequência de Fibonacci.");
+}
+else
+{
+    Console.WriteLine($"O número {numero} não pertence à sequência de Fibonacci.");
+}
